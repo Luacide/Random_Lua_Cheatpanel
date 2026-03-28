f45cef1b4c3a1ffd63c3641fbeb2f017596f688f
Luacide <gsdfgrsedfsd@gmail.com>
2026-03-28 14:42:38 -0400

    Modify script loader to include external script execution
    Change teleport condition and add script loading functionality.

--- ScriptLoader.lua
+++ ScriptLoader.lua
@@ -1,18 +1,17 @@
 -- Script Loader
-
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
 
 local gameid = game.GameId
 local maingameid = 13770948093
 
-if gameid ~= maingameid then
+if gameid == maingameid then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Luacide/Random_Lua_Cheatpanel/refs/heads/main/Main.lua"))()
+else
     local player = Players.LocalPlayer
-
     local success, err = pcall(function()
         TeleportService:TeleportAsync(maingameid, { player })
     end)
-
     if not success then
         warn("Teleport failed: " .. tostring(err))
     end
